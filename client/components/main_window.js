17cd5cd4000722ba71c8960cbaecee401cc0de26
Kifemi <kifemi@outlook.com>
2021-10-27 11:08:29 +0300

    Evidence collection connected to the app

--- client/components/main_window.js
+++ client/components/main_window.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Ghosts } from '../../imports/collections/ghosts';
+import { Evidence } from '../../imports/collections/evidence';
 
 function getGhosts(props, id) {
-    let ghost = props.ghosts.find(ghost => ghost.id == id);
+    let ghost = props[0].ghosts.find(ghost => ghost.id == id);
     if(ghost) {
         return ghost;
     } else {
@@ -12,17 +13,31 @@ function getGhosts(props, id) {
     }
 }
 
+function getEvidence(props, id) {
+    let evidence = props[1].evidence.find(evidence => evidence.id == id);
+    if(evidence) {
+        return evidence;
+    } else {
+        return { id: 0, name: "" }
+    }
+}
+
 function MainWindow(props) {
     ghost = getGhosts(props, 1)
+    evidence = getEvidence(props, 1)
     return (
         <div>
             <h1>Name: {ghost.name} </h1>
+            <h1>Name: {evidence.name} </h1>
         </div>
     );
 };
 
 export default withTracker(() => {
-    Meteor.subscribe('ghosts');
+    Meteor.subscribe('ghostsAndEvidence');
 
-    return { ghosts: Ghosts.find({}).fetch() };
+    return [
+        { ghosts: Ghosts.find({}).fetch() },
+        { evidence: Evidence.find({}).fetch() },
+    ];
 })(MainWindow);
--- server/main.js
+++ server/main.js
@@ -1,20 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Ghosts } from '../imports/collections/ghosts';
+import { Evidence } from '../imports/collections/evidence';
 
 Meteor.startup(() => {
 	// Check how many ghosts are in the collection
   	const numberOfGhosts = Ghosts.find({}).count();
+	const numberOfEvidence = Evidence.find({}).count()
 
 	// If no ghosts in the collection, insert them there
 	if(!numberOfGhosts) {
    	 	Ghosts.insert({
 			id: 1, name: "Spirit", evidences: [1,3,6], Str: "nothing", Weak: "Smudge"
-    	})
+    	});
   	};
 
-	Meteor.publish('ghosts', function() {
+	if(!numberOfEvidence) {
+		Evidence.insert({
+			id: 1, name: "EMF Level 5"
+		});
+	};
+
+	Meteor.publish('ghostsAndEvidence', function() {
 		// returns cursor, which is like a bookmark; not the actual data, but path to it
-		return Ghosts.find({}, { limit: 20 });
+		return [
+			Ghosts.find({}, { limit: 20 }),
+			Evidence.find({}, { limit: 20 }),
+		];
 	});
 });
